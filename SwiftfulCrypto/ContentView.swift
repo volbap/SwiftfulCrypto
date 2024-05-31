fe2e906d913e2377f49c979ccd68a02afaa31822
Pablo Villar <pablolvillar@gmail.com>
2024-05-31 16:08:17 -0300

    Module 1 done

--- SwiftfulCrypto/ContentView.swift
+++ SwiftfulCrypto/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+
+            VStack {
+                Text("Hello World")
+            }
         }
-        .padding()
     }
 }
 
---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Pablo Villar on 31/05/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
